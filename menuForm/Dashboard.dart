3e29539307b94ff3200e36a3f1ceb5db1350afda
SpicyLemon2803 <perezmyco28@gmail.com>
2019-11-19 02:05:05 +0800

    menu

---
+++ menuForm/Dashboard.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  Dashboard({Key key}) : super(key: key);
+
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body: Container(
+         child: Center(
+           child: Text('Dashboard Here'),
+         ),
+       ),
+    );
+  }
+}
