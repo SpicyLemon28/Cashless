26ef74e818f0c9e23183fc9a92f55f89bc2fdd3a
Conrad Perez <conrad_perez@yahoo.com>
2019-12-28 18:11:29 +0800

    04_refactorLoginPage

--- loginForm/LoginPage.dart
+++ loginForm/LoginPage.dart
@@ -5,11 +5,8 @@ import './ForgetPin.dart';
 import '../menuForm/NavBar.dart';
 import './RegisterPage.dart';
 
-
-
 class LoginPage extends StatefulWidget {
   LoginPage({Key key}) : super(key: key);
-
   @override
   _LoginPageState createState() => _LoginPageState();
 }
@@ -22,8 +19,8 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   void initState() {
+		super.initState();
     passwordVisible = false;
-    super.initState();
 }
 
   @override
@@ -42,11 +39,11 @@ class _LoginPageState extends State<LoginPage> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         appName('SmartPay'),
-                        passNreg('Sign Up', () => registerPage()),
-                        txtFormField(Icons.phone_android, 'Phone Number', TextInputType.number),
-                        txtFormField(Icons.lock, 'Password', TextInputType.text),
-                        passNreg('Forgot Password?', () => forgotPassDialog()),
-                        login('Sign In'),
+                        linkButton('Sign Up', () => navigatePage(Register())),
+                        textFormField(Icons.person, 'Phone Number', TextInputType.number),
+                        textFormField(Icons.lock, 'Password', TextInputType.text),
+                        linkButton('Forgot Password?', () => forgotPassDialog()),
+                        loginButton('Sign In'),
                       ],
                     ),
                   ),
@@ -59,48 +56,39 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-//Name of the App
-  Widget appName(txt) => Padding(
+	// Name of the App
+  Widget appName(txtApp) => Padding(
     padding: const EdgeInsets.only(top: 200, bottom: 50),
-    child: Text(txt, style: TextStyle(fontSize: 30),),
-    
+    child: Text(txtApp, style: TextStyle(fontSize: 30),),
   );
 
-  //Forgot Password and Sign Up Button
-  Widget passNreg(txt, onClick) => Padding(
+  // Forgot Password and Sign Up Button
+  Widget linkButton(txtLink, onClick) => Padding(
     padding: const EdgeInsets.only(left: 150),
     child: FlatButton(
-      child: Text(txt, style: TextStyle(fontSize: 12, color: Colors.blue)),
+      child: Text(txtLink, style: TextStyle(fontSize: 12, color: Colors.blue)),
       onPressed: onClick
     ),
   );
 
-  Widget txtFormField(icnTxt, hntTxt, keyType) => Padding(
+  Widget textFormField(icnText, hntText, keyType) => Padding(
     padding: const EdgeInsets.only(left: 20, right: 20, top: 10,),
     child: TextFormField(
       keyboardType: keyType,
-      obscureText: hntTxt == 'Password' ? !passwordVisible : false,
-      validator: (String value,) => textValidate(hntTxt, value),
+      obscureText: hntText == 'Password' ? !passwordVisible : false,
+      validator: (String value,) => textValidation(hntText, value),
       decoration: InputDecoration(
-        hintText: hntTxt,
-        prefixIcon: Icon(icnTxt, color: Colors.grey),
-        suffixIcon: hntTxt == 'Password' ? IconButton(
-          icon: Icon(
-            passwordVisible 
-            ? Icons.visibility
-            : Icons.visibility_off
-            ),
-          onPressed: () {
-            setState(() {
-              passwordVisible = !passwordVisible;
-            });
-          },
-        ):IconButton(
-          icon: Icon(
-            Icons.check_box_outline_blank),
-            onPressed: (){},
-            color: Colors.white
-            ),
+        hintText: hntText,
+        prefixIcon: Icon(icnText, color: Colors.grey),
+        suffixIcon: hntText == 'Password'
+					? IconButton(
+						icon: Icon(passwordVisible ? Icons.visibility : Icons.visibility_off),
+						onPressed: () => setState(() { passwordVisible = !passwordVisible; })
+					)
+					: IconButton(
+						icon: Icon(Icons.phone_android),
+						onPressed: () {}
+          ),
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(15),
         ),
@@ -108,101 +96,83 @@ class _LoginPageState extends State<LoginPage> {
     ),
   );
 
- //Login Button 
-  Widget login(txt) => Padding(
+  // Login Button
+  Widget loginButton(txtLogin) => Padding(
     padding: const EdgeInsets.only(top: 15),
     child: RaisedButton(
+			shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       color: Colors.greenAccent,
-      child: Text(txt),
-      onPressed: () => textValidated(),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-    ),
+      child: Text(txtLogin),
+      onPressed: () {
+				if (_formKey.currentState.validate()) navigatePage(NavBar());
+			}
+    )
   );
 
-//Functions
-
-
-  textValidate(hntTxt, value){
+	// Functions
+  textValidation(hntText, value) {
     var userInput = '123456';
+		var errorMessages;
 
-    switch (hntTxt){
-      case 'Phone Number':
-        if(value.isEmpty){
-          return 'Phone Number should not be empty';
-        }
-
-        if(value.length < 11){
-          return 'Phone Number should be 11 digits';
-        }
-      break;
-
-      case 'Password':
-        if(value != userInput){
-          return ('Incorrect Password');
-        }
-      break;
-    }
+		if (value.isEmpty) {
+			errorMessages = '$hntText should not be empty';
+		} else {
+			switch (hntText){
+				case 'Phone Number':
+					if (value.length < 11) errorMessages = 'Phone Number should be 11 digits';
+					break;
+				case 'Password':
+					if (value != userInput) errorMessages = 'Incorrect Password';
+					break;
+			}
+		}
+		return errorMessages;
   }
 
-  textValidated() {
-    if(_formKey.currentState.validate()){
-      Navigator.of(context).push(
-        CupertinoPageRoute<Null>(
-          builder: (
-            BuildContext context) => NavBar()));
-    }
-  }
+	void navigatePage(navTo) => Navigator.push(
+		context, CupertinoPageRoute(
+			builder: (context) => navTo
+		)
+	);
 
-  registerPage() => Navigator.of(context).push(
-    CupertinoPageRoute<Null>(
-      builder: (BuildContext context) => Register()  
-      )
-    );
-
-  forgotPassDialog() { 
-   return showDialog(
-    context: context,
-    builder: (BuildContext context) => AlertDialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      backgroundColor: Colors.grey[100],
-      title: Column(
-        children: <Widget>[
-          Text('We sent you a Confirmation Code',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 8.0),
-            child: Text('Please Check your Inbox',
-            textAlign: TextAlign.center, 
-            style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic),),
-          )
-        ],
-      ),
-      content: TextField(
-        decoration: InputDecoration(
-          hintText: 'Enter Confirmation Code',
-          hintStyle: TextStyle(fontSize: 12),
-          prefixIcon: Icon(Icons.code),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12)
-          ),
-        ),
-      ),
-      actions: <Widget>[
-        FlatButton(
-          child: Text('OKAY'),
-          onPressed: (){
-            Navigator.of(context).push(
-              CupertinoPageRoute<Null>(
-                builder: (
-                  BuildContext context) => ForgetPin()));
-            },
-          ),
-        ],
-      ),
-    );
-  }
+  forgotPassDialog() => showDialog(
+		context: context,
+		builder: (BuildContext context) => AlertDialog(
+			shape: RoundedRectangleBorder(
+				borderRadius: BorderRadius.circular(20),
+			),
+			backgroundColor: Colors.grey[100],
+			title: Column(
+				children: <Widget>[
+					Text('We sent you a Confirmation Code',
+						textAlign: TextAlign.center,
+						style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
+					),
+					Padding(
+						padding: const EdgeInsets.only(top: 8.0),
+						child: Text('Please Check your Inbox',
+							textAlign: TextAlign.center,
+							style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic)
+						)
+					)
+				]
+			),
+			content: TextField(
+				decoration: InputDecoration(
+					hintText: 'Enter Confirmation Code',
+					hintStyle: TextStyle(fontSize: 12),
+					prefixIcon: Icon(Icons.code),
+					enabledBorder: OutlineInputBorder(
+						borderRadius: BorderRadius.circular(12)
+					),
+				),
+			),
+			actions: <Widget>[
+				FlatButton(
+					child: Text('OKAY'),
+					onPressed: () => navigatePage(ForgetPin())
+				),
+			],
+		),
+	);
 }
--- main.dart
+++ main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import './loginForm/LoginPage.dart';
 
-void main () => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  title: "Cashless",
-  home: LoginPage(),
-  
+void main () => runApp(
+	MaterialApp(
+  	debugShowCheckedModeBanner: false,
+  	title: "Cashless",
+  	home: LoginPage()
   )
 );
