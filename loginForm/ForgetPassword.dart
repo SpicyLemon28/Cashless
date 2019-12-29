b57a1d07fd8b8142be67cfa1853eaee001fd8b81
Conrad Perez <conrad_perez@yahoo.com>
2019-12-29 16:19:39 +0800

    06_refactorForgetPassword

---
+++ loginForm/ForgetPassword.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'LoginPage.dart';
+
+class ForgetPin extends StatefulWidget {
+  ForgetPin({Key key}) : super(key: key);
+
+  @override
+  _ForgetPinState createState() => _ForgetPinState();
+}
+
+class _ForgetPinState extends State<ForgetPin> {
+
+	var _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              textPage('Create a New Pin'),
+              textFormField('New Password', 'Enter new password'),
+              textFormField('Re-confirm Password', 'Re-type new password'),
+              confirmButton('Confirm')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget textPage(lblText) => Padding(
+    padding: const EdgeInsets.only(bottom: 50,),
+    child: Text(lblText, style: TextStyle(fontSize: 25)),
+  );
+
+  Widget textFormField(lblText, hntText) => Padding(
+    padding: const EdgeInsets.only(top: 10, bottom: 10),
+    child: TextFormField(
+			obscureText: true,
+			validator: (String value,) => textValidation(lblText, value),
+      decoration: InputDecoration(
+        labelText: lblText,
+        hintText: hntText,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    ),
+  );
+
+  Widget confirmButton(lblText) => Padding(
+    padding: const EdgeInsets.only(top: 10),
+    child: RaisedButton(
+      child: Text(lblText),
+      onPressed: () {
+				if (_formKey.currentState.validate()) {
+					Navigator.of(context).push(
+						CupertinoPageRoute<Null>(builder: (
+						BuildContext context) => LoginPage())
+					);
+				}
+			}
+    ),
+  );
+
+  textValidation(lblText, value) {
+		var errorMessages;
+
+		if (value.isEmpty) {
+			errorMessages = '$lblText should not be empty';
+		} else {
+			if (value.length < 16) errorMessages = '$lblText should be 16 digits';
+		}
+
+		return errorMessages;
+  }
+
+}
