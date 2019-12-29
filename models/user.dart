c2acbdabeff1e276ff1fe8f90cea85c8b60eb63d
SpicyLemon2803 <perezmyco28@gmail.com>
2019-12-29 14:42:07 +0800

    usermodel update

--- models/user.dart
+++ models/user.dart
@@ -1,5 +1,8 @@
 
 class User {
-  int student_id, user_id, pin;
+  int studentId, userId, pin, token, phone;
   String name, email, password;
+
+  User(this.studentId, this.userId, this.pin, this.token, this.phone,
+       this.name, this.email, this.password);
 }
