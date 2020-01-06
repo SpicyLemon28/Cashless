74cc034ce711937be49e33894ae96345a4b06e71
SpicyLemon2803 <perezmyco28@gmail.com>
2020-01-06 22:23:29 +0800

    refractor Database

--- models/user.dart
+++ models/user.dart
@@ -1,43 +1,43 @@
-
 class User {
-  int studId, userId, pin, phone;
-  String name, email, password, date;
 
-  User (this.name, this.email, this.phone, this.studId, this.password, this.pin, this.date);
+  int id;
+  String name, email, phone, studentId, password, pin, date;
 
-  User.withId (this.userId, this.name, this.email, this.phone, this.studId, this.password, this.pin, this.date);
+  User(this.name, this.email, this.phone, this.studentId, this.password, this.pin, this.date);
+
+  User.withId(this.id, this.name, this.email, this.phone, this.studentId, this.password, this.pin, this.date);
 
   User.fromJson(Map json)
-    : userId      = json['userId'],
-      name        = json['name'],
-      email       = json['email'],
-			phone       = json['phone'],
-      studId      = json['studentId'],
-      password    = json['password'],
-			pin         = json['pin'],
-      date        = json['date'];
-  
+    : id        = json['id'],
+      name      = json['name'],
+      email     = json['email'],
+			phone     = json['phone'],
+      studentId = json['studentId'],
+      password  = json['password'],
+			pin       = json['pin'],
+      date      = json['date'];
+
   Map<String, dynamic> toMap() {
 		var map = Map<String, dynamic>();
-		if (userId != null) map['userId'] = userId;
-    map['name']       = name;
-    map['email']      = email;
-		map['phone']      = phone;
-		map['studId']     = studId;
-    map['password']   = password;
-		map['pin']        = pin;
-    map['date']       = date;
+		if (id != null) map['id'] = id;
+    map['name']      = name;
+    map['email']     = email;
+		map['phone']     = phone;
+		map['studentId'] = studentId;
+    map['password']  = password;
+		map['pin']       = pin;
+    map['date']      = date;
 
 		return map;
 	}
 
   User.fromDb(Map map)
-		: userId      = map['userId'],
-      name        = map['name'],
-      email       = map['email'],
-			phone       = map['phone'],
-      studId      = map['studId'],
-      password    = map['password'],
-			pin         = map['pin'],
-      date        = map['date'];   
+		: id        = map['id'],
+      name      = map['name'],
+      email     = map['email'],
+			phone     = map['phone'],
+      studentId = map['studentId'],
+      password  = map['password'],
+			pin       = map['pin'],
+      date      = map['date'];
 }
