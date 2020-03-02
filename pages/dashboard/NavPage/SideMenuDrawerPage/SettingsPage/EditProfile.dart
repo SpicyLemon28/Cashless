d6c17430aae5d9f1a7afa6c01236f991d608220d
SpicyLemon2803 <perezmyco28@gmail.com>
2020-03-02 21:59:13 +0800

    added user profile

--- pages/dashboard/NavPage/SideMenuDrawerPage/SettingsPage/EditProfile.dart
+++ pages/dashboard/NavPage/SideMenuDrawerPage/SettingsPage/EditProfile.dart
@@ -8,6 +8,9 @@ class EditProfile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<EditProfile> {
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -20,9 +23,134 @@ class _EditProfileState extends State<EditProfile> {
             onPressed: () => navigatePreviousPage('/settings'),
           ),
         ),
+        body: Form(
+          key: _formKey,
+          child: Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(bottom: 580),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 130),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(),
+                    child: Container(
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(left: 30),
+                            child: text('User Name', TextStyle(fontSize: 20, fontWeight: FontWeight.w500)),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 70),
+                            child: text('User Phone Number', TextStyle(color: Colors.grey [700], fontSize: 14, fontWeight: FontWeight.w400)),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 90, 260, 520),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey [700],
+                        blurRadius: 5,
+                        spreadRadius: 2
+                      )
+                    ]
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 230),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white
+                  ),
+                  child: ListView.builder(
+                    itemCount: 1,
+                    itemBuilder: (context, index) => Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(right: 60),
+                          child: title('Email:'),
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10, right: 10, bottom: 20),
+                          child: Card(
+                            elevation: 3,
+                            child: listTile('example@email.com'),
+                          ),
+                        ),
+
+                         title('Password:'),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10, right: 10, bottom: 20),
+                          child: Card(
+                            elevation: 3,
+                            child: listTile('12345678'),
+                          ),
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.only(right: 70),
+                          child: title('Pin:'),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10, right: 10),
+                          child: Card(
+                            elevation: 3,
+                            child: listTile('123456'),
+                          ),
+                        ),
+                      ],
+                    )
+                    ),
+                ),
+              )
+            ],
+          )
+        ),
       ),
     );
   }
+  Widget title(txt) => Padding(
+    padding: const EdgeInsets.only(right: 230),
+    child: Text(txt, style: TextStyle(color: Colors.grey [600], fontSize: 20, fontWeight: FontWeight.w500)),
+  );
+
+  Widget text(txt, styleText) => Padding(
+    padding: const EdgeInsets.only(bottom: 5),
+    child: Text(txt, style: styleText,),
+  );
+
+  Widget listTile(txt) => ListTile(
+    title: Text(txt, style: TextStyle(color: Colors.grey, fontSize: 14)),
+    onTap: (){},
+    trailing: Icon(Icons.edit),
+  );
+
+  Widget editIcon(iconTxt) => Padding(
+    padding: const EdgeInsets.only(left: 350),
+    child: Icon(iconTxt),
+  );
 
   void navigatePage(navTo) =>
 		Navigator.pushReplacementNamed(context, navTo);
