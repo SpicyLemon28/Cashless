9bf9c062ec5f1df77017e2386217a4a0960459b5
SpicyLemon2803 <perezmyco28@gmail.com>
2020-04-02 23:22:42 +0800

    bckEndComment

--- pages/dashboard/NavPage/SideMenuDrawerPage/SettingsPage/EditProfilePage/EditProfile.dart
+++ pages/dashboard/NavPage/SideMenuDrawerPage/SettingsPage/EditProfilePage/EditProfile.dart
@@ -8,7 +8,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../../../../../../utilities/registration_utilities.dart';
 import '../../../../../../global.dart';
 
-
 class EditProfile extends StatefulWidget {
   EditProfile({Key key}) : super(key: key);
 
@@ -18,18 +17,8 @@ class EditProfile extends StatefulWidget {
 
 class _EditProfileState extends State<EditProfile> {
 
-  RegistrationUtilities register = RegistrationUtilities();
+  var _phone, _fullname, _email , _confirmationCode;
 
-  var _phone, _fullname, _email;
-
-  final _formKey = GlobalKey<FormState>();
-  final scaffoldKey = GlobalKey<ScaffoldState>();
-
-  bool _isLoading = false;
-  bool _autoValidate = false;
-
-	String _phone;
-	String _confirmationCode;
 
   getPref() async {
     SharedPreferences preferences = await SharedPreferences.getInstance();
@@ -59,30 +48,25 @@ class _EditProfileState extends State<EditProfile> {
             onPressed: () => navigatePreviousPage(context),
           ),
         ),
-        key: scaffoldKey,
-        body: Form(
-          key: _formKey,
-          autovalidate: _autoValidate,
-          child: Stack(
-              fit:StackFit.expand,
-              children: <Widget>[
-                ListView(
-                  children: <Widget>[
-                    title('Name'),
-                    card(_fullname, Icon(Icons.edit, color: Colors.grey), () => navigatePage('/changeName')),
-                    title('Phone Number'),
-                    card(securePhone(_phone), Icon(Icons.phone_android, color: Colors.grey), null),
-                    title('Email'),
-                    card(_email, Icon(Icons.email, color: Colors.grey), null),
-                    title('Password'),
-                    card('********', Icon(Icons.edit, color: Colors.grey), () => cfmPassDialog()),
-                    title('Pin'),
-                    card('********', Icon(Icons.edit, color: Colors.grey), () => cfmPinDialog()),
-                  ],
-                )
-              ],
-            ),
-        ),
+        body: Stack(
+            fit:StackFit.expand,
+            children: <Widget>[
+              ListView(
+                children: <Widget>[
+                  title('Name'),
+                  card(_fullname, Icon(Icons.edit, color: Colors.grey), () => navigatePage('/changeName')),
+                  title('Phone Number'),
+                  card(securePhone(_phone), Icon(Icons.phone_android, color: Colors.grey), null),
+                  title('Email'),
+                  card(_email, Icon(Icons.email, color: Colors.grey), null),
+                  title('Password'),
+                  card('********', Icon(Icons.edit, color: Colors.grey), () => cfmPassDialog()),//_verifyForgetPassword('CHANGE PASSWORD', 'Password')
+                  title('Pin'),
+                  card('********', Icon(Icons.edit, color: Colors.grey), () => cfmPinDialog()),//_verifyForgetPassword('CHANGE PIN', 'Pin')
+                ],
+              )
+            ],
+          ),
         ),
     );
   }
@@ -92,25 +76,16 @@ class _EditProfileState extends State<EditProfile> {
     child: Text(txt, style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),),
   );
 
-  Widget card(listTxt, suffixIcon, goTo) {
-    return Padding(padding: const EdgeInsets.only(),
-    child: _isLoading 
-    ? CircularProgressIndicator(
-        valueColor: AlwaysStoppedAnimation(Theme.of(context).primaryColor),
-      )
-    : Card(
-      elevation: 5,
-      color: Colors.white,
-      child: ListTile(
-        trailing: suffixIcon,
-        onTap: goTo,
-        title: Text(listTxt, style: TextStyle(color: Colors.grey),
-          )
-        ),
-      )
-    );
-  }
-  
+  Widget card(listTxt, suffixIcon, goTo) => Card(
+    elevation: 5,
+    color: Colors.white,
+    child: ListTile(
+      trailing: suffixIcon,
+      onTap: goTo,
+      title: Text(listTxt, style: TextStyle(color: Colors.grey),),
+    ),
+  );
+
   void navigatePage(navTo) =>
 		Navigator.pushReplacementNamed(context, navTo);
 
@@ -120,18 +95,7 @@ class _EditProfileState extends State<EditProfile> {
 		return phone == null ? "" : phone.replaceRange(4, 9, '*' * 5);
 	}
 
-  void _submit() {
-		final form = _formKey.currentState;
-		if (form.validate()) {
-			setState(() => _isLoading = true);
-			form.save();
-			_verifyForgetPassword();
-		} else {
-			setState(() => _autoValidate = true);
-		}
-	}
-
-  void _verifyForgetPassword() async {
+  /*void _verifyForgetPassword(txtLabel, navFor) async {
   	var data = { "phone" : _phone };
 
     http.Response response = await http.post(REQUEST_RESET_PASSWORD, body: data);
@@ -140,39 +104,16 @@ class _EditProfileState extends State<EditProfile> {
 		setState(() => _isLoading = false);
     if (response.statusCode == 200) {
 			int result = responseData['result'];
-			if (result == 1) cfmPassDialog();
+			if (result == 1) dialog();
 		} else {
     	register.snackBarShow(scaffoldKey, responseData['error']);
 		}
-  }
+  } */
 
-  void _verifyConfirmationCode() async {
-  	var data = { "confirmationCode" : _confirmationCode };
-
-    http.Response response = await http.post(CONFIRMED_REQUEST_RESET_PASSWORD, body: data);
-    final responseData = json.decode(response.body);
-
-		setState(() => _isLoading = false);
-    if (response.statusCode == 200) {
-			int result = responseData['result'];
-			if (result == 2) {
-				savePref(_phone, responseData['token']);
-        Navigator.pushReplacementNamed(context, '/changePass');
-			}
-		} else {
-			register.showAlertDialog(context, 'Error', responseData['error']);
-		}
-  }
-
-  savePref(String phone, String token) async {
+  /*savePref(String token) async {
     SharedPreferences preferences = await SharedPreferences.getInstance();
-    setState(() {
-			preferences.setString("phone", phone);
-      preferences.setString("token", token);
-    });
-  }
-
-
+    setState(() => preferences.setString("token", token));
+  } */
 
   //Password Confirmation Code
   cfmPassDialog() => showDialog(
@@ -188,7 +129,6 @@ class _EditProfileState extends State<EditProfile> {
     ],),
     content: TextField(
       autofocus: true,
-      onChanged: (value) {_confirmationCode = value;},
       decoration: InputDecoration(
         hintText: 'Enter Confirmation Code',
         hintStyle: TextStyle(fontSize: 12),
@@ -199,10 +139,53 @@ class _EditProfileState extends State<EditProfile> {
     actions: <Widget>[
       FlatButton(
         child: Text('SUBMIT'),
-        onPressed: () => _verifyConfirmationCode()//navigatePage('/changePass'),
+        onPressed: () => navigatePage('/changePass'),
       )
     ],
   ));
+
+  /*cfmDialog(String txtLabel, String navFor) => showDialog(
+    context: context,
+    builder: (BuildContext context) => AlertDialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20),
+    ),
+    title: Column(children: <Widget>[
+      Text(txtLabel, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+      Padding(padding: const EdgeInsets.only(top: 10)),
+      Text('To proceed with your request, please enter your confirmation code sent to your email:',
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal)),
+    ],),
+    content: TextField(
+      autofocus: true,
+      decoration: InputDecoration(
+        hintText: 'Enter Confirmation Code',
+        hintStyle: TextStyle(fontSize: 12),
+        prefixIcon: Icon(Icons.code),
+        enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+      ),
+    ),
+    actions: <Widget>[
+      FlatButton(
+        child: Text('SUBMIT'),
+        onPressed: () => _verifyConfirmationCode(),
+      )
+    ],
+  ));
+   */
+
+  /*void _verifyConfirmationCode(navFor) async {
+  	var data = { "confirmationCode" : _confirmationCode };
+
+    http.Response response = await http.post(CONFIRMED_REQUEST_RESET_PASSWORD, body: data);
+    final responseData = json.decode(response.body);
+
+    if (navFor == 'Password') {
+      Navigator.pushReplacementNamed(context, '/changePass');
+    }
+    if (navFor == 'Pin') {
+      Navigator.pushReplacementNamed(context, '/changePin');
+    }
+  } */
 
   //Pin Confirmation Code
   cfmPinDialog() => showDialog(
